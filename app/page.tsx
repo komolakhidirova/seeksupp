b97d912b9c25849daf689dc36435083684a9d9e1
Komola Khidirova <komolakhidirova@gmail.com>
2025-05-31 15:53:52 +0500

    all posts

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,10 @@
-import PostCard from '@/components/PostCard'
-import { getAllPosts } from '@/lib/actions/post.actions'
+import AllPosts from '@/components/AllPosts'
 
-const Page = async () => {
-	const posts = await getAllPosts({ limit: 10 })
-
+const Page = () => {
 	return (
 		<>
 			<h1>Forum</h1>
-
-			<section className='mt-9 flex flex-col gap-10'>
-				{posts.map(post => (
-					<PostCard key={post.id} {...post} authorId={post.author} />
-				))}
-			</section>
+			<AllPosts />
 		</>
 	)
 }
